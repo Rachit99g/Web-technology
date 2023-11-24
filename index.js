3e10d30cccf05ce808ec8334fd82c32efdd455e2
Rachit99g <132730344+Rachit99g@users.noreply.github.com>
2023-11-25 00:18:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+/*a = document.getElementById('btn')
+b = document.getElementById('head')
+let flag = 0;
+a.addEventListener("click",function(){
+    if(flag==0){
+        b.style.color='red'
+        console.log("jal gya ab band kr de")
+        flag = 1;
+    }
+    else{
+        b.style.color='black'
+        flag=0;
+        console.log("band kyu hai bsdk jala to shi")
+    }
+})*/
